9139a36784a4fb06243d6b21850d4ae47a2ef9fc
loclieu <loclieu1234@gmail.com>
2016-09-18 00:52:57 +1000

    Assignment
    f

--- Deakin Helper/Deakin Helper/Deakin_Helper.Droid/Resources/Resource.Designer.cs
+++ Deakin Helper/Deakin Helper/Deakin_Helper.Droid/Resources/Resource.Designer.cs
@@ -3199,88 +3199,88 @@ namespace Deakin_Helper.Droid
 			public const int abc_textfield_search_material = 2130837565;
 			
 			// aapt resource value: 0x7f02003e
-			public const int contacts = 2130837566;
+			public const int Add = 2130837566;
 			
 			// aapt resource value: 0x7f02003f
-			public const int design_fab_background = 2130837567;
+			public const int contacts = 2130837567;
 			
 			// aapt resource value: 0x7f020040
-			public const int design_snackbar_background = 2130837568;
+			public const int design_fab_background = 2130837568;
 			
 			// aapt resource value: 0x7f020041
-			public const int hamburger = 2130837569;
+			public const int design_snackbar_background = 2130837569;
 			
 			// aapt resource value: 0x7f020042
-			public const int ic_cast_dark = 2130837570;
+			public const int hamburger = 2130837570;
 			
 			// aapt resource value: 0x7f020043
-			public const int ic_cast_disabled_light = 2130837571;
+			public const int ic_cast_dark = 2130837571;
 			
 			// aapt resource value: 0x7f020044
-			public const int ic_cast_light = 2130837572;
+			public const int ic_cast_disabled_light = 2130837572;
 			
 			// aapt resource value: 0x7f020045
-			public const int ic_cast_off_light = 2130837573;
+			public const int ic_cast_light = 2130837573;
 			
 			// aapt resource value: 0x7f020046
-			public const int ic_cast_on_0_light = 2130837574;
+			public const int ic_cast_off_light = 2130837574;
 			
 			// aapt resource value: 0x7f020047
-			public const int ic_cast_on_1_light = 2130837575;
+			public const int ic_cast_on_0_light = 2130837575;
 			
 			// aapt resource value: 0x7f020048
-			public const int ic_cast_on_2_light = 2130837576;
+			public const int ic_cast_on_1_light = 2130837576;
 			
 			// aapt resource value: 0x7f020049
-			public const int ic_cast_on_light = 2130837577;
+			public const int ic_cast_on_2_light = 2130837577;
 			
 			// aapt resource value: 0x7f02004a
-			public const int ic_media_pause = 2130837578;
+			public const int ic_cast_on_light = 2130837578;
 			
 			// aapt resource value: 0x7f02004b
-			public const int ic_media_play = 2130837579;
+			public const int ic_media_pause = 2130837579;
 			
 			// aapt resource value: 0x7f02004c
-			public const int ic_media_route_disabled_mono_dark = 2130837580;
+			public const int ic_media_play = 2130837580;
 			
 			// aapt resource value: 0x7f02004d
-			public const int ic_media_route_off_mono_dark = 2130837581;
+			public const int ic_media_route_disabled_mono_dark = 2130837581;
 			
 			// aapt resource value: 0x7f02004e
-			public const int ic_media_route_on_0_mono_dark = 2130837582;
+			public const int ic_media_route_off_mono_dark = 2130837582;
 			
 			// aapt resource value: 0x7f02004f
-			public const int ic_media_route_on_1_mono_dark = 2130837583;
+			public const int ic_media_route_on_0_mono_dark = 2130837583;
 			
 			// aapt resource value: 0x7f020050
-			public const int ic_media_route_on_2_mono_dark = 2130837584;
+			public const int ic_media_route_on_1_mono_dark = 2130837584;
 			
 			// aapt resource value: 0x7f020051
-			public const int ic_media_route_on_mono_dark = 2130837585;
+			public const int ic_media_route_on_2_mono_dark = 2130837585;
 			
 			// aapt resource value: 0x7f020052
-			public const int ic_pause_dark = 2130837586;
+			public const int ic_media_route_on_mono_dark = 2130837586;
 			
 			// aapt resource value: 0x7f020053
-			public const int ic_pause_light = 2130837587;
+			public const int ic_pause_dark = 2130837587;
 			
 			// aapt resource value: 0x7f020054
-			public const int ic_play_dark = 2130837588;
+			public const int ic_pause_light = 2130837588;
 			
 			// aapt resource value: 0x7f020055
-			public const int ic_play_light = 2130837589;
+			public const int ic_play_dark = 2130837589;
 			
 			// aapt resource value: 0x7f020056
-			public const int ic_setting_dark = 2130837590;
+			public const int ic_play_light = 2130837590;
 			
 			// aapt resource value: 0x7f020057
-			public const int ic_setting_light = 2130837591;
+			public const int ic_setting_dark = 2130837591;
 			
 			// aapt resource value: 0x7f020058
-			public const int icon = 2130837592;
+			public const int ic_setting_light = 2130837592;
 			
 			// aapt resource value: 0x7f020059
-			public const int icon_addClasses = 2130837593;
+			public const int icon = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
 			public const int icon_assignments = 2130837594;
